5cda47f4767245c9476b9cf91cdb38152c837f27
Maciej <m.nikiel@gmail.com>
2020-05-12 23:43:37 +0200

    Add printing to txt form main app
    Add printing to txt form main app
    
    Add printing to txt form main app

--- Aplikacja.py
+++ Aplikacja.py
@@ -3,10 +3,12 @@ from tkinter.filedialog import askopenfilename
 
 from labeling import labeling
 from ordfilt2 import ordfilt2
-from regionprops_cent import regionprops_cent
-from regionprops_cent import regionprops_equiv_diam
+from regionprops import regionprops_cent
+from regionprops import regionprops_equiv_diam
 from PIL import Image
+import numpy as np
 import os
+
 ################LABELING
 Tk().withdraw()
 filename1 = askopenfilename()
@@ -30,11 +32,13 @@ im_ordfilt = ordfilt2(filename2, m_x, m_y, n)
 file_name = '%s_ordfil.png' % (os.path.basename(filename2).split(".")[0])
 im_ordfilt.save(file_name)
 
-
 #################REGIONPROPS
 im = Image.open("logic_image_labeled.png")
+
 im_prop_cent = regionprops_cent(im)
-im_prop_cent = regionprops_equiv_diam(im)
-#regionprops_cent(im_labeled)
+file_name = '%s_prop_centroid.txt' % (os.path.basename(filename1).split(".")[0])
+np.savetxt(file_name, im_prop_cent, fmt='%1.6g')
 
-
+im_prop_equiv_diam = regionprops_equiv_diam(im)
+file_name = '%s_prop_equiv_diam.txt' % (os.path.basename(filename1).split(".")[0])
+np.savetxt(file_name, im_prop_equiv_diam, fmt='%1.6g')
