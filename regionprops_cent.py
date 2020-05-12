d3e0fdf7ab4ae930fe201fb0371692a2530e6226
Maciej <m.nikiel@gmail.com>
2020-05-12 23:24:34 +0200

    Create region props diameter equivalent

--- regionprops_cent.py
+++ regionprops_cent.py
@@ -1,13 +1,14 @@
 import numpy as np
 from PIL import Image
+from math import sqrt
+from math import pi
 
 
 def regionprops_cent(image):
     pix = np.array(image)
 
     unique = np.unique(pix)
-    unique = unique[unique != 0] # dla czarnego nie liczymy
-
+    unique = unique[unique != 0]  # dla czarnego nie liczymy
 
     # tworzenie macierzy na wyniki
     output = np.zeros((unique.size, 3))
@@ -23,5 +24,30 @@ def regionprops_cent(image):
         output[i, 1] = output[i, 1] / length + 1
         output[i, 2] = output[i, 2] / length + 1
         i = i + 1
+
+    print(unique)
+
+
+def regionprops_equiv_diam(image):
+    pix = np.array(image)
+
+    unique = np.unique(pix)
+    unique = unique[unique != 0]  # dla czarnego nie liczymy
+
+    # tworzenie macierzy na wyniki
+    output = np.zeros((unique.size, 2))
+    output[:, 0] = unique
+
+    area = np.zeros((unique.size, 1))
+
+    for i in range(len(unique)):
+        where = np.argwhere(pix == unique[i])
+        area[i] = where.size / 2
+        i = i + 1
+
+    factor = 2/sqrt(pi)
+
+    for i in range(len(unique)):
+        output[i, 1] = sqrt(area[i]) * factor
 
     print(unique)
