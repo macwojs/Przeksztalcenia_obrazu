1434888d1046f1602374e78b296e93f944393403
Maciej <m.nikiel@gmail.com>
2020-05-15 20:45:55 +0200

    Dilate mono work

--- morfo_mono.py
+++ morfo_mono.py
@@ -11,6 +11,14 @@ def find_ero(mask, cut):
                 output = np.append(output, [cut[row, col]])
     return output
 
+def find_dil(mask, cut):
+    output = np.array([0])
+    for row in range(mask.shape[0]):
+        for col in range(mask.shape[1]):
+            if mask[row, col] == 1:
+                output = np.append(output, [cut[row, col]])
+    return output
+
 def erode_mono(im, len, deg):
     strel = strel_line(len, deg)
 
@@ -49,9 +57,50 @@ def erode_mono(im, len, deg):
     # image_out[image_out > 0] = 255
     image_out = np.array(image_out, np.uint8)
     new_image = Image.fromarray(image_out)
+    new_image.save("images/new2.png")
+    return new_image
+
+def dilate_mono(im, len, deg):
+    strel = strel_line(len, deg)
+
+    # strel = np.ones((3, 3))
+    strel_row_around = strel.shape[0] - 1
+    strel_col_around = strel.shape[1] - 1
+    strel_row_one_side = int(strel_row_around / 2)
+    strel_col_one_side = int(strel_col_around / 2)
+
+    # # image = np.ones((13, 15))
+    # # image[1, 7] = 0
+    # image = Image.open(im)
+    image = np.array(im)
+    # image[image > 0] = 1
+
+    # powiększenie rozszerzenie obrazu o zera
+    image_big = np.zeros((image.shape[0] + strel_row_around, image.shape[1] + strel_col_around))
+    #image_big[image_big > 0] = 0
+    image_big[strel_row_one_side:image.shape[0] + strel_row_one_side,
+    strel_col_one_side:image.shape[1] + strel_col_one_side] = image
+
+    # stworzenie obrazu wyjściowego
+    image_out = np.zeros((image.shape[0], image.shape[1]))
+
+    for row in range(strel_row_one_side, image.shape[0] + strel_row_one_side):
+        for col in range(strel_col_one_side, image.shape[1] + strel_col_one_side):
+            image_cut = image_big[row - strel_row_one_side:row + strel_row_one_side + 1,
+                        col - strel_col_one_side:col + strel_col_one_side + 1]
+            values = find_dil(strel, image_cut)
+            image_out[row - strel_row_one_side, col - strel_col_one_side] = np.amax(values)
+
+    # im = Image.open("logic_image.png")
+    # pix = np.array(im)
+    # pix[pix > 0] = 1 # zmiana na obraz binarny
+
+    # image_out[image_out > 0] = 255
+    image_out = np.array(image_out, np.uint8)
+    new_image = Image.fromarray(image_out)
     new_image.save("images/new3.png")
     return new_image
 
-
 ime = Image.open("images/cameraman.tif")
 image_dil = erode_mono(ime, 12, 120)
+image_ero = dilate_mono(ime, 12, 120)
