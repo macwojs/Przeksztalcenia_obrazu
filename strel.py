2d493351d7e71eb9abec7357b924ff07579d4f8a
Maciej <m.nikiel@gmail.com>
2020-05-22 21:20:10 +0200

    Refromat strel.py

--- strel.py
+++ strel.py
@@ -2,22 +2,26 @@ import numpy as np
 from bresenham import bresenham
 
 
-def strel_line(length, degrees):
+def strel_line(length, deg):
     if length >= 1:
-        theta = degrees * np.pi / 180
-        x = round((length - 1) / 2 * np.cos(theta))
-        y = -round((length - 1) / 2 * np.sin(theta))
+        th = deg * np.pi / 180
+        x = round((length - 1) / 2 * np.cos(th))
+        y = -round((length - 1) / 2 * np.sin(th))
+
         points = list(bresenham(int(-x), int(-y), int(x), int(y)))
+
         points_x = [point[0] for point in points]
-        points_y = [point[1] for point in points]
-        n_rows = int(2 * max([abs(point_y) for point_y in points_y]) + 1)
-        n_columns = int(2 * max([abs(point_x) for point_x in points_x]) + 1)
-        strel = np.zeros((n_rows, n_columns))
-        rows = ([point_y + max([abs(point_y) for point_y in points_y]) for point_y in points_y])
+        col_n = int(2 * max([abs(point_x) for point_x in points_x]) + 1)
         columns = ([point_x + max([abs(point_x) for point_x in points_x]) for point_x in points_x])
-        idx = []
+
+        points_y = [point[1] for point in points]
+        row_n = int(2 * max([abs(point_y) for point_y in points_y]) + 1)
+        rows = ([point_y + max([abs(point_y) for point_y in points_y]) for point_y in points_y])
+
+        ind = []
+        strel = np.zeros((row_n, col_n))
         for x in zip(rows, columns):
-            idx.append(np.ravel_multi_index((int(x[0]), int(x[1])), (n_rows, n_columns)))
-        strel.reshape(-1)[idx] = 1
+            ind.append(np.ravel_multi_index((int(x[0]), int(x[1])), (row_n, col_n)))
+        strel.reshape(-1)[ind] = 1
 
     return strel
